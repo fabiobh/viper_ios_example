9dc6cc4146b3dacf81f6fe8fc2110e4b8acd0d39
fabiobh <fabiobh@hotmail.com.br>
2025-11-24 11:18:58 -0300

    New project setup

---
+++ ios_viper_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_viper_example
+//
+//  Created by Fabio Cunha on 24/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
