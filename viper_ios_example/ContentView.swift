64cd8eeb5207fc7f71e5106f9b3d8aca4dd6a9fc
fabiobh <fabiobh@hotmail.com.br>
2025-11-24 11:05:54 -0300

    initial setup

---
+++ viper_ios_example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  viper_ios_example
+//
+//  Created by Fabio Cunha on 24/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ viper_ios_example/viper_ios_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  viper_ios_exampleApp.swift
+//  viper_ios_example
+//
+//  Created by Fabio Cunha on 24/11/25.
+//
+
+import SwiftUI
+
+@main
+struct viper_ios_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
